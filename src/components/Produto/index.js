9b67425d18e55958741cb0bc979b4bf25e998499
Thalita Andrade <thalita.andrade287@gmail.com>
2019-10-22 18:30:51 -0300

    alinhando meios de pagamento

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -20,34 +20,37 @@ const HeaderProduto = styled.div `
 `
 const MainProduto = styled.div `
 	display: grid;
-	grid-template-columns: 2fr 2fr  1fr;
-	padding-right: 20px;
-	padding-left: 20px;
+	justify-items: center;
+	grid-template-columns: 1fr auto  1fr;
 `
 
 const ContainerFoto = styled.div `
-	width: 300px;
-	height: 400px;
+	width: 20vw;
+	height: 40vh;
 	border: 1px solid black;
 `
 const ContainerDescricao = styled.div `
 
 `
 const ContainerMeiosPag = styled.div  `
-	width: 300px;
-	height: 200px;
+	width: 20vw;
+	height: 20vh;
 	border: 1px solid black;
+	display: flex;
+	flex-direction:column;
+	justify-content: center;
+	align-items: center;
 `
 
 const DescricaoProduto = styled.div `
-	width: 400px;
-	height: 300px;
+	width: 40vw;
+	height: 30vh;
 	border: 1px solid black;
 `
 const ContainerFotos = styled.div `
 	display: flex;
-	padding-right: 20px;
-	padding-left: 20px;
+	justify-content: flex-start;
+	padding-left: 5vw;
 `
 const Foto1 = styled.div `
 	width: 100px;
@@ -100,17 +103,19 @@ class Produto extends React.Component {
 
 					<ContainerMeiosPag>
 						<h3>Meios de Pagamento</h3>
-
-						<label htmlFor="selectPagamento">Número de parcelas:</label>
-						<select id="selectPagamento">
-							<option>Selecione</option>
-							<option>1</option>
-							<option>2</option>
-							<option>3</option>
-							<option>4</option>
-							<option>5</option>
-						</select>
-						<h3>Preço:</h3>
+						<div>
+							<label htmlFor="selectPagamento">Número de parcelas:  </label>
+							<select id="selectPagamento">
+								<option>Selecione</option>
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5</option>
+							</select>
+						
+							<h3>Preço:</h3>
+						</div>
 						<button>Comprar</button>
 					</ContainerMeiosPag>
 
