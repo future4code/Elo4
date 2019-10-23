77401c136912763558ad32eaa7332611a9d97f4f
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-10-23 11:27:24 -0300

    1 passo pagina do comprador

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import CardProduct from './sectionProduct/cardProduct'
+
 
 export class AppContainer extends React.Component {
 	constructor(props) {
@@ -6,8 +8,10 @@ export class AppContainer extends React.Component {
 	}
 
 	render() {
-		return <div>
-			
-		</div>
+		return (
+			<div>
+				<CardSection/>
+			</div>
+		);
 	}
 }
---
+++ src/components/sectionProduct/cardProduct/index.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+class CardProduct extends React.Component {
+	constructor(props){
+		super(props)
+	}
+
+	render(){
+		return(
+			<div>
+				
+			</div>
+		);
+	}
+}
