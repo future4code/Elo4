a6062cbf55d07db5b773be178f18128ecbf7525f
Christhopher Porpino <cgporpinof@gmail.com>
2019-10-24 11:15:16 -0300

    primeira versão da home estilizada. corrigir fonte dos botões

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import Home from './Homepage/Home'
-import logoElo4 from '../assets/imgs/logo1.png'
+import elo4Nome from '../assets/imgs/elo4Nome.png'
 import Consumidor from './Consumidor/'
 import Vendedor from './Vendedor/'
 
 const Logo = styled.img`
-	width: 150px;
+	width: 100px;
 	cursor: pointer;
+	margin: 10px 30px;
 `
 
 export class AppContainer extends React.Component {
@@ -19,11 +20,11 @@ export class AppContainer extends React.Component {
 	}
 
 	getCurrentPage = () => {
-		switch(this.state.currentPage){
+		switch (this.state.currentPage) {
 			case 'Vendedor':
-				return <Vendedor/>
+				return <Vendedor />
 			case 'Consumidor':
-				return <Consumidor/>
+				return <Consumidor />
 			case 'Home':
 				return <Home onChangePage={this.goToPage} />
 			default:
@@ -44,7 +45,7 @@ export class AppContainer extends React.Component {
 
 		return (
 			<Fragment>
-				<Logo onClick={this.goBackToHome} src={logoElo4} alt="logoElo4" />
+				<Logo onClick={this.goBackToHome} src={elo4Nome} alt="logoElo4" />
 				{selectedPage}
 			</Fragment>
 		)
--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -4,25 +4,29 @@ import styled from 'styled-components'
 import Produto from '../Produto/'
 import Consumidor from '../Consumidor/'
 import Vendedor from '../Vendedor/'
+import Button from '@material-ui/core/Button';
 
 const Root = styled.div`
   display:flex;
   flex-direction: column;
   width: 1280px;
+  height: 91vh;
+  background-color: #E7E2FC;
 `
 
 const MainContent = styled.div`
 	display: flex;
 	flex-direction: column;
 	width: 100%;
-	height: 95vh;
+	height: 100%;
 	justify-content: center;
 	align-items: center;
 `
 
 const ChooseButton = styled.div`
 	display: flex;
-	
+	width: 400px;
+	justify-content: space-between;
 `
 
 // const ButtonConsumer = styled.div``
@@ -33,8 +37,12 @@ const Home = (props) => {
 		<Root>
 			<MainContent>
 				<ChooseButton>
-					<button onClick={() => props.onChangePage('Vendedor')}>Quero Vender</button> 
-					<button onClick={() => props.onChangePage('Consumidor')}>Quero Comprar</button>
+					<Button variant="contained" size="large" onClick={() => props.onChangePage('Vendedor')}>
+						Quero Vender
+        			</Button>
+					<Button variant="contained" size="large" color="primary" onClick={() => props.onChangePage('Consumidor')}>
+						Quero Comprar
+        			</Button>
 				</ChooseButton>
 			</MainContent>
 		</Root>
