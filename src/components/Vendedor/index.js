23618a3e18d2af020cbf935475acaa6c11f00d0b
Brunna Rocha <brunna.17rocha03@gmail.com>
2019-10-24 17:52:24 -0300

    Criando as funções de onChange com API

--- src/components/Vendedor/index.js
+++ src/components/Vendedor/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
 
 const Title = styled.h1 `
 	display: flex;
@@ -7,12 +8,81 @@ const Title = styled.h1 `
 `
 
 export default class Vendedor extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+		selectCategoria: '',
+		descricaoProduto: '',
+		preco: '',
+		selectPayment: '',
+		url: [],
+		parcelas: '',
+		}	
+	}
+
+	
+	addNewProduct = () => {
+		const data = {
+			category: this.state.selectCategoria,
+			description: this.state.descricaoProduto,
+			price: this.state.preco,
+			paymentMethod: this.state.selectPayment,
+			photos: this.state.url,
+			installments: this.state.parcelas,
+		}
+	
+		const request = axios
+			.post("https://us-central1-missao-newton.cloudfunctions.net/elo4/products",
+			data, {
+	})
+		request
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
+
+	onChangeCategory = (event) => {
+		this.setState({selectCategoria: event.target.value})
+	}
+	
+	onChangeDescriptionProduct = (event) => {
+		this.setState({descricaoProduto: event.target.value})
+	}
+
+	onChangePriceProduct = (event) => {
+		this.setState({preco: event.target.value})
+	}
+
+	onChangePayment = (event) => {
+		this.setState({selectPayment: event.target.value})
+	}
+
+
+	onChangeUrl = (event) => {
+		this.setState({url: event.target.value})
+	}
+
+	onChangeInstallments = (event) => {
+		this.setState({parcelas: event.target.value})
+	}
+
+
 	render() {
 		return (
 			<div>
 				<Title>Vendedor</Title>
 				<label htmlFor="selectCategoria">Categoria:  </label>
-				<select id="selectCategoria">
+
+				<select 
+					id="selectCategoria" 
+					onChange={this.onChangeCategory} 
+					value={this.state.selectCategoria}
+				>
+					<option>Selecione</option>
 					<option>Decoração</option>
 					<option>Saúde e Bem Estar</option>
 					<option>Brinquedos</option>
@@ -20,40 +90,56 @@ export default class Vendedor extends React.Component {
 
 				<div>
 					<p>Descrição do produto: </p>
-					<input type="text-area"/>
+					<input 
+						type="text-area" 
+						onChange={this.onChangeDescriptionProduct} 
+						value={this.state.descricaoProduto}
+					/>
 				</div>
 
 				<div>
 					<p>Preço:</p>
-					<input type="text"/>
+					<input 
+						type="number" 
+						onChange={this.onChangePriceProduct} 
+						value={this.state.preco}
+					/>
 				</div>
 
 				<div>
 					<p>Meios de pagamento:</p>
-					<label htmlFor="selectPagamentoCCard">Cartão de crédito</label>
-					<input type="checkbox"/>
-					<label htmlFor="selectPagamentoDCard">Cartão de débito</label>
-					<input type="checkbox"/>
-					<label htmlFor="selectPagamentoBol">Boleto</label>
-					<input type="checkbox"/>
+					<select
+					 	id="selectPayment" 
+						onChange={this.onChangePayment} 
+						value={this.state.selectPayment}
+					>
+						<option>Selecione</option>						
+						<option>Cartão de crédito</option>
+						<option>Cartão de débito</option>
+						<option>Boleto bancário</option>
+					</select>
+				</div>
+
+				<div>
+					<p>Parcelas</p>
+					<input 
+						type="number" 
+						onChange={this.onChangeInstallments} 
+						value={this.state.parcelas}
+					/>
 				</div>
 
 				<div>
 					<p>Adicionar URL da primeira imagem:</p>
-					<input type="text"/>
+					<input 
+						type="text" 
+						id="url" 
+						onChange={this.onChangeUrl} 
+						value={this.state.url}
+					/>
 				</div>
 
-				<div>
-					<p>Adicionar URL da segunda imagem:</p>
-					<input type="text"/>
-				</div>
-
-				<div>
-					<p>Adicionar URL da terceira imagem:</p>
-					<input type="text"/>
-				</div>
-
-				<button>Submeter!</button>
+				<button onClick={this.addNewProduct}>Submeter!</button>
 
 			</div>
 
